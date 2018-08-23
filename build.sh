d3d421f93eec2eaeb47e15e941435234504fa5c4
bob <38596466+bobbae@users.noreply.github.com>
2018-08-22 19:28:00 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ echo building
 cd example
 make
 docker build -t bobbae/example .
-docker push bobbae/example
+# push will fail if you didn't login before
+#docker push bobbae/example
 
 echo hello 1 2 3 4 5
